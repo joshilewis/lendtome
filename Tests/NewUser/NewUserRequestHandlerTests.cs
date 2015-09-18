b8150a9112085d59ee98d18318c4ff88f8b7de49
Joshua Lewis <joshua.lewis@dstvdm.com>
2015-09-18 10:22:23 +0200

    NewUserRequestHandler now under test

---
+++ Tests/NewUser/NewUserRequestHandlerTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lending.Core;
+using Lending.Execution.Auth;
+using NUnit.Framework;
+using ServiceStack.Authentication.NHibernate;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+using ServiceStack.ServiceInterface.Auth;
+
+namespace Tests.NewUser
+{
+    public class NewUserRequestHandlerTests : DatabaseFixtureBase
+    {
+        [Test]
+        public void ExistingUserShouldNotBeCreated()
+        {
+            var existingUser = DefaultTestData.ServiceStackUser1;
+            SaveEntities(existingUser);//This is needed because ServiceStack will persist the AuthDto behind the scenes on sign-up
+
+            CommitTransactionAndOpenNew();
+
+            var request = new AuthSessionDouble();
+            var expectedResponse = new BaseResponse();
+
+            var expectedUser = DefaultTestData.ServiceStackUser1;
+
+            var sut = new NewUserRequestHandler(() => Session);
+            BaseResponse actualResponse = sut.HandleRequest(request);
+
+            actualResponse.ShouldEqual(expectedResponse);
+
+            CommitTransactionAndOpenNew();
+
+            int numberOfUsersInDb = Session
+                .QueryOver<ServiceStackUser>()
+                .RowCount()
+                ;
+
+            Assert.That(numberOfUsersInDb, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void NewUserShouldBeCreated()
+        {
+            var authDto = DefaultTestData.UserAuthPersistenceDto1;
+            SaveEntities(authDto);//This is needed because ServiceStack will persist the AuthDto behind the scenes on sign-up
+
+            CommitTransactionAndOpenNew();
+
+            var request = new AuthSessionDouble();
+            var expectedResponse = new BaseResponse();
+
+            var expectedUser = DefaultTestData.ServiceStackUser1;
+
+            var sut = new NewUserRequestHandler(() => Session);
+            BaseResponse actualResponse = sut.HandleRequest(request);
+
+            actualResponse.ShouldEqual(expectedResponse);
+
+            CommitTransactionAndOpenNew();
+
+            ServiceStackUser userInDb = Session
+                .QueryOver<ServiceStackUser>()
+                .SingleOrDefault()
+                ;
+
+            userInDb.ShouldEqual(expectedUser);
+
+        }
+
+        public class AuthSessionDouble : IAuthSession
+        {
+            public bool HasRole(string role)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool HasPermission(string permission)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsAuthorized(string provider)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnRegistered(IServiceBase registrationService)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnAuthenticated(IServiceBase authService, IAuthSession session, IOAuthTokens tokens, Dictionary<string, string> authInfo)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnLogout(IServiceBase authService)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnCreated(IHttpRequest httpReq)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string ReferrerUrl { get; set; }
+            public string Id { get; set; }
+
+            public string UserAuthId
+            {
+                get { return "1"; }
+                set { } 
+            }
+
+            public string UserAuthName { get; set; }
+            public string UserName { get; set; }
+            public string DisplayName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public List<IOAuthTokens> ProviderOAuthAccess { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public DateTime LastModified { get; set; }
+            public List<string> Roles { get; set; }
+            public List<string> Permissions { get; set; }
+            public bool IsAuthenticated { get; set; }
+            public string Sequence { get; set; }
+        }
+    }
+}
