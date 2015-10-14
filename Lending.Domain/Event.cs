2926c2593631e6486f3a9bf1a0e78f5865738596
Joshua Lewis <joshua.lewis@dstvdm.com>
2015-10-14 11:09:00 +0200

    Event now has Id property

--- Lending.Domain/Event.cs
+++ Lending.Domain/Event.cs
@@ -4,11 +4,13 @@ namespace Lending.Domain
 {
     public abstract class Event
     {
+        public Guid Id { get; set; }
         public Guid AggregateId { get; set; }
         public Guid ProcessId { get; set; }
 
         protected Event(Guid processId, Guid aggregateId)
         {
+            Id = Guid.NewGuid();
             AggregateId = aggregateId;
             ProcessId = processId;
         }
--- Lending.Domain/EventRoute.cs
+++ Lending.Domain/EventRoute.cs
@@ -8,16 +8,16 @@ namespace Lending.Domain
 {
     public class EventRoute<T> : IEventRoute
     {
-        private Action<T> handler { get; set; }
+        private Action<T> Handler { get; set; }
         public Type HandlerType { get; private set; }
         public EventRoute(Action<T> handler, Type handlerType)
         {
-            this.handler = handler;
+            this.Handler = handler;
             this.HandlerType = handlerType;
         }
         public void Handle(object @event)
         {
-            handler((dynamic)@event);
+            Handler((dynamic)@event);
         }
         public override string ToString()
         {
