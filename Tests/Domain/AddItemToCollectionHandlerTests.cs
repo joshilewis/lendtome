79f7bceb214f1acdf71fd624631bf4f5584ecaf5
Joshua Lewis <joshua.lewis@dstvdm.com>
2015-10-15 16:17:25 +0200

    Added blank failing tests for AddItemToCollectionHandler

---
+++ Tests/Domain/AddItemToCollectionHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests.Domain
+{
+    public class AddItemToCollectionHandlerTests : FixtureWithEventStoreAndNHibernate
+    {
+        [Test]
+        public void AddingNonExistentItemToCollectionShouldCreateItemAndAddToCollection()
+        {
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void AddingNewExistingItemToCollectionShouldAddToCollection()
+        {
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void AddingDuplicateExistingItemToCollectionShouldFail()
+        {
+
+            Assert.Fail();
+        }
+
+    }
+}
