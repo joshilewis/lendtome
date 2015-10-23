7c0be75605a6f989cb315c6915c2c67eadeb16ca
Joshua Lewis <joshua.lewis@dstvdm.com>
2015-10-23 12:16:23 +0200

    Previous commit: Corrected names and summaries to match #9

--- Tests/Domain/AddBookToLibraryHandlerTests.cs
+++ Tests/Domain/AddBookToLibraryHandlerTests.cs
@@ -18,54 +18,16 @@ namespace Tests.Domain
     public class AddBookToLibraryHandlerTests : FixtureWithEventStoreAndNHibernate
     {
         /// <summary>
-        /// GIVEN User1 is a Registered User AND Book1 is not Published
-        /// WHEN User1 adds Book1 to her Collection
-        /// THEN Book1 is Published in the system AND Book1 is added to User1's Collection
+        /// GIVEN User1 is a Registered User
+        /// WHEN User1 Adds Book1 to her Library
+        /// THEN Book1 is Added to User1's Library
         /// </summary>
         [Test]
-        public void AddingUnpublishedBookToCollectionShouldPublishBookAndAddToCollection()
+        public void AddingNewBookToLibraryShouldSucceed()
         {
             var processId = Guid.NewGuid();
             var user1 = User.Register(processId, Guid.NewGuid(), "User 1", "email1");
             SaveAggregates(user1);
-
-            var registeredUser1 = new RegisteredUser(user1.Id, user1.UserName);
-            SaveEntities(registeredUser1);
-
-            var command = new AddBookToCollection(processId, user1.Id, user1.Id, "Book1", "Author1", "ISBNNumber");
-            var expectedResult = new Result();
-            var bookAddedToLibrary = new BookAddedToLibrary(processId, user1.Id, command.Title, command.Author, command.Isbn);
-
-            var sut = new AddBookToCollectionHandler(() => Session, () => Repository);
-            Result actualResult = sut.HandleCommand(command);
-            CommitTransactionAndOpenNew();
-            WriteRepository();
-
-            actualResult.ShouldEqual(expectedResult);
-
-            StreamEventsSlice  slice = Connection.ReadStreamEventsForwardAsync($"user-{user1.Id}", 0, 10, false).Result;
-            Assert.That(slice.Events.Length, Is.EqualTo(2));
-            var value = Encoding.UTF8.GetString(slice.Events[1].Event.Data);
-            BookAddedToLibrary actualAddedToLibrary = value.FromJson<BookAddedToLibrary>();
-            actualAddedToLibrary.ShouldEqual(bookAddedToLibrary);
-
-        }
-
-        /// <summary>
-        /// GIVEN User1 is a Registered User AND Book1 is not Published
-        /// WHEN User1 adds Book1 to her Collection
-        /// THEN Book1 is Published in the system AND Book1 is added to User1's Collection
-        /// </summary>
-        [Test]
-        public void AddingPublishedBookToCollectionShouldAddToCollection()
-        {
-            var processId = Guid.NewGuid();
-            var user1 = User.Register(processId, Guid.NewGuid(), "User 1", "email1");
-            SaveAggregates(user1);
-
-            var registeredUser1 = new RegisteredUser(user1.Id, user1.UserName);
-            SaveEntities(registeredUser1);
-            CommitTransactionAndOpenNew();
 
             var command = new AddBookToCollection(processId, user1.Id, user1.Id, "Title", "Author", "Isbn");
             var expectedResult = new Result();
@@ -87,21 +49,17 @@ namespace Tests.Domain
         }
 
         /// <summary>
-        /// GIVEN User1 is a registered user AND Book1 is Published AND Book1 is in User1's Collection
-        /// WHEN User1 adds Book1 to her Collection
-        /// THEN User1 is notified that Book1 is already in her Collection
+        /// GIVEN User1 is a Registered User AND Book1 is in User1's Library
+        /// WHEN User1 Adds Book1 to her Library
+        /// THEN User1 is notified that Book1 is already in her Library
         /// </summary>
         [Test]
-        public void AddingPublishedBookAlreadyInCollectionShouldFail()
+        public void AddingDuplicateBookToLibraryShouldFail()
         {
             var processId = Guid.NewGuid();
             var user1 = User.Register(processId, Guid.NewGuid(), "User 1", "email1");
             user1.AddBookToLibrary(processId, "Title", "Authority", "Isbn");
             SaveAggregates(user1);
-
-            var registeredUser1 = new RegisteredUser(user1.Id, user1.UserName);
-            SaveEntities(registeredUser1);
-            CommitTransactionAndOpenNew();
 
             var command = new AddBookToCollection(processId, user1.Id, user1.Id, "Title", "Authority", "Isbn");
             var expectedResult = new Result(User.BookAlreadyInLibrary);
