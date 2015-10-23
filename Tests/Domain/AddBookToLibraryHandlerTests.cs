570810318c434b33cbe0d7eecc06e1cc40bad598
Joshua Lewis <joshilewis@gmail.com>
2015-10-23 16:11:39 +0200

    Added scenario for AddBookToLibrary #9

--- Tests/Domain/AddBookToLibraryHandlerTests.cs
+++ Tests/Domain/AddBookToLibraryHandlerTests.cs
@@ -15,6 +15,10 @@ using ServiceStack.Text;
 
 namespace Tests.Domain
 {
+    /// <summary>
+    /// https://github.com/joshilewis/lending/issues/9
+    /// As a User I want to Add Books to my Library so that my Connections can see what Books I own.
+    /// </summary>
     public class AddBookToLibraryHandlerTests : FixtureWithEventStoreAndNHibernate
     {
         /// <summary>
@@ -73,6 +77,39 @@ namespace Tests.Domain
 
             StreamEventsSlice  slice = Connection.ReadStreamEventsForwardAsync($"user-{user1.Id}", 0, 10, false).Result;
             Assert.That(slice.Events.Length, Is.EqualTo(2));
+
+        }
+
+        /// <summary>
+        /// GIVEN User1 is a Registered User AND User1 has Added and Removed Book1 from her Library
+        /// WHEN User1 Adds Book1 to her Library
+        /// THEN Book1 is Added to User1's Library
+        /// </summary>
+        [Test]
+        public void AddingPreviouslyRemovedBookToLibraryShouldSucceed()
+        {
+            var processId = Guid.NewGuid();
+            var user1 = User.Register(processId, Guid.NewGuid(), "User 1", "email1");
+            user1.AddBookToLibrary(processId, "Title", "Author", "Isbn");
+            user1.RemoveBookFromLibrary(processId, "Title", "Author", "Isbn");
+            SaveAggregates(user1);
+
+            var command = new AddBookToLibrary(processId, user1.Id, user1.Id, "Title", "Author", "Isbn");
+            var expectedResult = new Result();
+            var expectedBookAddedToCollection = new BookAddedToLibrary(processId, user1.Id, command.Title, command.Author, command.Isbn);
+
+            var sut = new AddBookToLibraryHandler(() => Session, () => Repository);
+            Result actualResult = sut.HandleCommand(command);
+            CommitTransactionAndOpenNew();
+            WriteRepository();
+
+            actualResult.ShouldEqual(expectedResult);
+
+            StreamEventsSlice slice = Connection.ReadStreamEventsForwardAsync($"user-{user1.Id}", 0, 10, false).Result;
+            Assert.That(slice.Events.Length, Is.EqualTo(4));
+            var value = Encoding.UTF8.GetString(slice.Events[1].Event.Data);
+            BookAddedToLibrary actualAddedToLibrary = value.FromJson<BookAddedToLibrary>();
+            actualAddedToLibrary.ShouldEqual(expectedBookAddedToCollection);
 
         }
 
