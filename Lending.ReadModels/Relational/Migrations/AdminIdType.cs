0d4dfc06d1bc1d995bdf0e8a88c8a943ee818a5b
Joshua Lewis <joshilewis@gmail.com>
2017-11-22 14:02:14 +0200

    fix: Changes type of column for library admin IDs to string for Firebase-based auth

---
+++ Lending.ReadModels/Relational/Migrations/AdminIdType.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using FluentMigrator;
+
+namespace Lending.ReadModels.Relational.Migrations
+{
+    [Migration(20171122)]
+    public class AdminIdType : Migration
+    {
+        public override void Up()
+        {
+            Alter.Column("administratorid").OnTable("OpenedLibrary").AsString();
+            Alter.Column("libraryadminid").OnTable("LibraryBook").AsString();
+            Alter.Column("acceptingadministratorid").OnTable("LibraryLink").AsString();
+            Alter.Column("requestingadministratorid").OnTable("LibraryLink").AsString();
+            Alter.Column("targetadministratorid").OnTable("RequestedLink").AsString();
+            Alter.Column("requestingadministratorid").OnTable("RequestedLink").AsString();
+        }
+
+        public override void Down()
+        {
+            //Alter.Column("administratorid").OnTable("OpenedLibrary").AsGuid();
+            //Alter.Column("libraryadminid").OnTable("LibraryBook").AsGuid();
+            //Alter.Column("acceptingadministratorid").OnTable("LibraryLink").AsGuid();
+            //Alter.Column("requestingadministratorid").OnTable("LibraryLink").AsGuid();
+            //Alter.Column("targetadministratorid").OnTable("RequestedLink").AsGuid();
+            //Alter.Column("requestingadministratorid").OnTable("RequestedLink").AsGuid();
+        }
+
+    }
+}
